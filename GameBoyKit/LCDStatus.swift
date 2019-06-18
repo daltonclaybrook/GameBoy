90ddfb1f91d5f61dd0df569b61427170f170d170
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-06-17 20:13:20 -0400

    Added accessors for the various flags on the LCDStatus register

--- GameBoyKit/LCDStatus.swift
+++ GameBoyKit/LCDStatus.swift
@@ -14,6 +14,31 @@ public struct LCDStatus: RawRepresentable {
 }
 
 extension LCDStatus {
+	var lcdYCoincidence: Bool {
+		get { return rawValue & 0x04 != 0 }
+		set { rawValue = newValue ? rawValue | 0x04 : rawValue & 0xfb }
+	}
+
+	var hBlankInterruptEnabled: Bool {
+		get { return rawValue & 0x08 != 0 }
+		set { rawValue = newValue ? rawValue | 0x08 : rawValue & 0xf7 }
+	}
+
+	var vBlankInterruptEnabled: Bool {
+		get { return rawValue & 0x10 != 0 }
+		set { rawValue = newValue ? rawValue | 0x10 : rawValue & 0xef }
+	}
+
+	var oamInterruptEnabled: Bool {
+		get { return rawValue & 0x20 != 0 }
+		set { rawValue = newValue ? rawValue | 0x20 : rawValue & 0xdf }
+	}
+
+	var lcdYCoincidenceInterruptEnabled: Bool {
+		get { return rawValue & 0x40 != 0 }
+		set { rawValue = newValue ? rawValue | 0x40 : rawValue & 0xbf }
+	}
+
 	var mode: Mode {
 		get { return Mode(rawValue: rawValue & 0x03)! }
 		set { rawValue = (rawValue & 0xfc) | newValue.rawValue }

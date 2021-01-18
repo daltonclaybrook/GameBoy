9d13f056ac1484bbf6ece13f435d405b66c9651b
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-01-17 21:15:53 -0500

    Implement stacked queued dma transfers

--- GameBoyKit/Memory/OAM.swift
+++ GameBoyKit/Memory/OAM.swift
@@ -1,3 +1,8 @@
+private struct QueuedDMATransfer {
+    let source: Byte
+    var cyclesUntilTransferStarts: Cycles
+}
+
 public final class OAM: MemoryAddressable {
     public weak var mmu: MMU?
     private var oamBytes = [Byte](repeating: 0, count: MemoryMap.OAM.count)
@@ -5,10 +10,12 @@ public final class OAM: MemoryAddressable {
     private let dmaTransferDuration: Cycles = 160
     private let dmaTransferStartDelay: Cycles = 2
 
-    private var queuedDMATransfer = false
+    /// A FIFO stack of requested DMA transfers. Because of the delay between
+    /// when a transfer is requested and when it begins, it is possible for
+    /// several transfers to get queued up one after another.
+    private var queuedTransfers: [QueuedDMATransfer] = []
     private var cyclesSinceStartOfTransfer: Cycles = 0
-    private var cyclesUntilTransferStarts: Cycles = 0
-    private var transferSource: Byte = 0
+    private var currentTransferSource: Byte = 0
 
     public func read(address: Address) -> Byte {
         if let mmu = mmu, mmu.isDMATransferActive {
@@ -23,32 +30,52 @@ public final class OAM: MemoryAddressable {
     }
 
     public func startDMATransfer(source: Byte) {
-        transferSource = source
-        cyclesSinceStartOfTransfer = 0
-        cyclesUntilTransferStarts = dmaTransferStartDelay
-        queuedDMATransfer = true
+        queuedTransfers.append(
+            QueuedDMATransfer(
+                source: source,
+                cyclesUntilTransferStarts: getStartDelayForQueuedDMATransfer()
+            )
+        )
     }
 
     public func emulate() {
         guard let mmu = mmu else { return }
-        if queuedDMATransfer {
-            emulateQueuedTransfer(mmu: mmu)
-        } else if mmu.isDMATransferActive {
-            emulateTransferActive(mmu: mmu)
-        }
+        emulateQueuedTransfers(mmu: mmu)
+        // begin immediately if transfer is activated in the step above
+        emulateActiveTransferIfNecessary(mmu: mmu)
     }
 
     // MARK: - Helpers
 
-    private func emulateQueuedTransfer(mmu: MMU) {
-        cyclesUntilTransferStarts -= 1
-        if cyclesUntilTransferStarts == 0 {
-            queuedDMATransfer = false
-            mmu.isDMATransferActive = true
+    private func getStartDelayForQueuedDMATransfer() -> Cycles {
+        if mmu?.isDMATransferActive ?? false {
+            // This might be a hack. The mooneye oam_dma_restart test seems to fail
+            // if the start delay is the same for a fresh start and a restart.
+            return dmaTransferStartDelay + 1
+        } else {
+            return dmaTransferStartDelay
         }
     }
 
-    private func emulateTransferActive(mmu: MMU) {
+    private func emulateQueuedTransfers(mmu: MMU) {
+        var index = 0
+        while index < queuedTransfers.count {
+            defer { index += 1 }
+            queuedTransfers[index].cyclesUntilTransferStarts -= 1
+
+            if queuedTransfers[index].cyclesUntilTransferStarts == 0 {
+                let transfer = queuedTransfers.remove(at: index)
+                index -= 1
+                currentTransferSource = transfer.source
+                cyclesSinceStartOfTransfer = 0
+                mmu.isDMATransferActive = true
+            }
+        }
+    }
+
+    private func emulateActiveTransferIfNecessary(mmu: MMU) {
+        guard mmu.isDMATransferActive else { return }
+
         cyclesSinceStartOfTransfer += 1
         if cyclesSinceStartOfTransfer >= dmaTransferDuration {
             performDMATransfer(mmu: mmu)
@@ -57,7 +84,7 @@ public final class OAM: MemoryAddressable {
     }
 
     private func performDMATransfer(mmu: MMU) {
-        let sourceAddress = Address(transferSource) * 0x100
+        let sourceAddress = Address(currentTransferSource) * 0x100
         (0..<Address(MemoryMap.OAM.count)).forEach { offset in
             let from = sourceAddress + offset
             let byte = mmu.read(address: from, privileged: true)

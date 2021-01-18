fefc5b80396b4cd97cb902b61054635d65ff8a8f
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-01-18 15:50:33 -0500

    Move bootstrap function to an extension

---
+++ GameBoyKit/Device/GameBoy+Bootstrap.swift
@@ -0,0 +1,45 @@
+extension GameBoy {
+    /// This is used to bypass the boot rom. All necessary CPU and memory
+    /// registers are updated with values that would normally be set by
+    /// the boot rom. Notably, the PC register is set to 0x100, which is
+    /// where the cartridge ROM takes over and begins execution.
+    func bootstrap() {
+        cpu.af = 0x01b0
+        cpu.bc = 0x0013
+        cpu.de = 0x00d8
+        cpu.sp = 0xfffe
+        cpu.pc = 0x100
+
+        mmu.write(byte: 0x00, to: 0xff05, privileged: true)
+        mmu.write(byte: 0x00, to: 0xff06, privileged: true)
+        mmu.write(byte: 0x00, to: 0xff07, privileged: true)
+        mmu.write(byte: 0x80, to: 0xff10, privileged: true)
+        mmu.write(byte: 0xbf, to: 0xff11, privileged: true)
+        mmu.write(byte: 0xf3, to: 0xff12, privileged: true)
+        mmu.write(byte: 0xbf, to: 0xff14, privileged: true)
+        mmu.write(byte: 0x3f, to: 0xff16, privileged: true)
+        mmu.write(byte: 0x00, to: 0xff17, privileged: true)
+        mmu.write(byte: 0xbf, to: 0xff19, privileged: true)
+        mmu.write(byte: 0x7f, to: 0xff1a, privileged: true)
+        mmu.write(byte: 0xff, to: 0xff1b, privileged: true)
+        mmu.write(byte: 0x9f, to: 0xff1c, privileged: true)
+        mmu.write(byte: 0xbf, to: 0xff1e, privileged: true)
+        mmu.write(byte: 0xff, to: 0xff20, privileged: true)
+        mmu.write(byte: 0x00, to: 0xff21, privileged: true)
+        mmu.write(byte: 0x00, to: 0xff22, privileged: true)
+        mmu.write(byte: 0xbf, to: 0xff23, privileged: true)
+        mmu.write(byte: 0x77, to: 0xff24, privileged: true)
+        mmu.write(byte: 0xf3, to: 0xff25, privileged: true)
+        mmu.write(byte: 0xf1, to: 0xff26, privileged: true)
+        mmu.write(byte: 0x91, to: 0xff40, privileged: true)
+        mmu.write(byte: 0x00, to: 0xff42, privileged: true)
+        mmu.write(byte: 0x00, to: 0xff43, privileged: true)
+        mmu.write(byte: 0x00, to: 0xff45, privileged: true)
+        mmu.write(byte: 0xfc, to: 0xff47, privileged: true)
+        mmu.write(byte: 0xff, to: 0xff48, privileged: true)
+        mmu.write(byte: 0xff, to: 0xff49, privileged: true)
+        mmu.write(byte: 0x00, to: 0xff4a, privileged: true)
+        mmu.write(byte: 0x00, to: 0xff4b, privileged: true)
+        mmu.write(byte: 0x00, to: 0xffff, privileged: true)
+    }
+}
--- GameBoyKit/Device/GameBoy.swift
+++ GameBoyKit/Device/GameBoy.swift
@@ -13,13 +13,13 @@ public final class GameBoy {
         label: "com.daltonclaybrook.GameBoy.GameBoy",
         qos: .userInteractive
     )
+    let cpu: CPU
+    let mmu: MMU
     private let clock: Clock
     private let timer: Timer
-    private let cpu: CPU
     private let oam: OAM
     private let ppu: PPU
     private let io: IO
-    private let mmu: MMU
 
     private let vram = VRAM()
     private let palette = ColorPalette()
@@ -89,50 +89,6 @@ public final class GameBoy {
         ppu.emulate()
         // emulate timer
         timer.emulate()
-    }
-
-    /// This is used to bypass the boot rom. All necessary CPU and memory
-    /// registers are updated with values that would normally be set by
-    /// the boot rom. Notably, the PC register is set to 0x100, which is
-    /// where the cartridge ROM takes over and begins execution.
-    private func bootstrap() {
-        cpu.af = 0x01b0
-        cpu.bc = 0x0013
-        cpu.de = 0x00d8
-        cpu.sp = 0xfffe
-        cpu.pc = 0x100
-
-        mmu.write(byte: 0x00, to: 0xff05, privileged: true)
-        mmu.write(byte: 0x00, to: 0xff06, privileged: true)
-        mmu.write(byte: 0x00, to: 0xff07, privileged: true)
-        mmu.write(byte: 0x80, to: 0xff10, privileged: true)
-        mmu.write(byte: 0xbf, to: 0xff11, privileged: true)
-        mmu.write(byte: 0xf3, to: 0xff12, privileged: true)
-        mmu.write(byte: 0xbf, to: 0xff14, privileged: true)
-        mmu.write(byte: 0x3f, to: 0xff16, privileged: true)
-        mmu.write(byte: 0x00, to: 0xff17, privileged: true)
-        mmu.write(byte: 0xbf, to: 0xff19, privileged: true)
-        mmu.write(byte: 0x7f, to: 0xff1a, privileged: true)
-        mmu.write(byte: 0xff, to: 0xff1b, privileged: true)
-        mmu.write(byte: 0x9f, to: 0xff1c, privileged: true)
-        mmu.write(byte: 0xbf, to: 0xff1e, privileged: true)
-        mmu.write(byte: 0xff, to: 0xff20, privileged: true)
-        mmu.write(byte: 0x00, to: 0xff21, privileged: true)
-        mmu.write(byte: 0x00, to: 0xff22, privileged: true)
-        mmu.write(byte: 0xbf, to: 0xff23, privileged: true)
-        mmu.write(byte: 0x77, to: 0xff24, privileged: true)
-        mmu.write(byte: 0xf3, to: 0xff25, privileged: true)
-        mmu.write(byte: 0xf1, to: 0xff26, privileged: true)
-        mmu.write(byte: 0x91, to: 0xff40, privileged: true)
-        mmu.write(byte: 0x00, to: 0xff42, privileged: true)
-        mmu.write(byte: 0x00, to: 0xff43, privileged: true)
-        mmu.write(byte: 0x00, to: 0xff45, privileged: true)
-        mmu.write(byte: 0xfc, to: 0xff47, privileged: true)
-        mmu.write(byte: 0xff, to: 0xff48, privileged: true)
-        mmu.write(byte: 0xff, to: 0xff49, privileged: true)
-        mmu.write(byte: 0x00, to: 0xff4a, privileged: true)
-        mmu.write(byte: 0x00, to: 0xff4b, privileged: true)
-        mmu.write(byte: 0x00, to: 0xffff, privileged: true)
     }
 
     private func processInterruptIfNecessary() {

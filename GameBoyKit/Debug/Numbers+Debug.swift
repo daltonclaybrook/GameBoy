d2d79a21b15043d17e12105b4907e36a2ea5c938
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-01-26 23:01:22 -0500

    Refactor Joypad

--- GameBoyKit/Debug/Numbers+Debug.swift
+++ GameBoyKit/Debug/Numbers+Debug.swift
@@ -2,10 +2,18 @@ public extension Byte {
     var hexString: String {
         String(format: "%02X", self)
     }
+
+    var binaryString: String {
+        String(self, radix: 2)
+    }
 }
 
 public extension Address {
     var hexString: String {
         String(format: "%02x", self)
+    }
+
+    var binaryString: String {
+        String(self, radix: 2)
     }
 }
--- GameBoyKit/Memory/Joypad.swift
+++ GameBoyKit/Memory/Joypad.swift
@@ -14,25 +14,20 @@ public final class Joypad: RawRepresentable {
         case start
     }
 
-    public enum SelectedKeys {
-        case directionKeys
-        case buttonKeys
-    }
-
     public private(set) var rawValue: UInt8
     public weak var delegate: JoypadDelegate?
 
     private var pressedKeys: Set<Key> = []
-    private var selectedKeys: SelectedKeys = .directionKeys
+    private var selectedKeysRegister: Register = .allKeysSelected
 
-    public init(rawValue: UInt8) {
-        self.rawValue = rawValue
+    public init?(rawValue: UInt8) {
+        assertionFailure("Though required by the protocol, this initializer should not be used")
+        return nil
     }
 
-    public convenience init() {
-        // Since a button is considered "selected" if its corresponding bit
-        // is 0, setting all bits high means no buttons are selected.
-        self.init(rawValue: 0xff)
+    public init() {
+        self.rawValue = 0x00
+        updateRawValue()
     }
 
     public func update(byte: Byte) {
@@ -44,25 +39,17 @@ public final class Joypad: RawRepresentable {
         // bitwise NOT the byte:
         // 0001_0000
         //    ^ indicates selection
-        //
-        // then bitwise AND with selection mask. Check if != 0.
-        // 0001_0000 & 0001_0000 != 0
         let invertedSelection = ~byte
-        if invertedSelection & SelectedKeys.directionKeys.selectionMask != 0 {
-            selectedKeys = .directionKeys
-        } else if invertedSelection & SelectedKeys.buttonKeys.selectionMask != 0 {
-            selectedKeys = .buttonKeys
-        }
 
+        // Bits 4 & 5 are used for selection, so mask is 0011_0000, i.e. 0x30
+        selectedKeysRegister = Register(rawValue: invertedSelection & 0x30)
         updateRawValue()
     }
 
     public func keyWasPressed(_ key: Key) {
         pressedKeys.insert(key)
         updateRawValue()
-        if selectedKeys.allKeysInSelection.contains(key) {
-            delegate?.joypadDidRequestInterrupt(self)
-        }
+        delegate?.joypadDidRequestInterrupt(self)
     }
 
     public func keyWasReleased(_ key: Key) {
@@ -73,47 +60,78 @@ public final class Joypad: RawRepresentable {
     // MARK: - Helpers
 
     private func updateRawValue() {
-        var rawValue = 0xff ^ selectedKeys.selectionMask
-        let keysInSelection = selectedKeys.allKeysInSelection
-        let pressedKeysInSelection = pressedKeys.intersection(keysInSelection)
-        pressedKeysInSelection.forEach { key in
-            rawValue ^= key.inputMask
+        var invertedRawValue = selectedKeysRegister.intersection(.allKeysSelected).rawValue
+        if selectedKeysRegister.contains(.directionKeysSelected) {
+            let pressedDirectionKeys = pressedKeys.intersection(Key.allDirectionKeys)
+            invertedRawValue |= pressedDirectionKeys.registerMask.rawValue
         }
-        self.rawValue = rawValue
+        if selectedKeysRegister.contains(.buttonKeysSelected) {
+            let pressedButtonKeys = pressedKeys.intersection(Key.allButtonKeys)
+            invertedRawValue |= pressedButtonKeys.registerMask.rawValue
+        }
+        self.rawValue = ~invertedRawValue
+    }
+}
+
+public extension Joypad {
+    struct Register: OptionSet {
+        public let rawValue: UInt8
+
+        // Direction keys
+        public static let right = Register(rawValue: 1 << 0)
+        public static let left = Register(rawValue: 1 << 1)
+        public static let up = Register(rawValue: 1 << 2)
+        public static let down = Register(rawValue: 1 << 3)
+        public static let allDirectionKeys: Register = [.right, .left, .up, .down]
+
+        // Button keys
+        public static let a = Register(rawValue: 1 << 0)
+        public static let b = Register(rawValue: 1 << 1)
+        public static let select = Register(rawValue: 1 << 2)
+        public static let start = Register(rawValue: 1 << 3)
+        public static let allButtonKeys: Register = [.a, .b, .select, .start]
+
+        // Selection
+        public static let directionKeysSelected = Register(rawValue: 1 << 4)
+        public static let buttonKeysSelected = Register(rawValue: 1 << 5)
+        public static let allKeysSelected: Register = [.directionKeysSelected, .buttonKeysSelected]
+
+        public init(rawValue: UInt8) {
+            self.rawValue = rawValue
+        }
     }
 }
 
 extension Joypad.Key {
-    var inputMask: Byte {
+    static let allDirectionKeys: Set<Joypad.Key> = [.right, .left, .up, .down]
+    static let allButtonKeys: Set<Joypad.Key> = [.a, .b, .select, .start]
+
+    var registerMask: Joypad.Register {
         switch self {
-        case .right, .a:
-            return 0b0001
-        case .left, .b:
-            return 0b0010
-        case .up, .select:
-            return 0b0100
-        case .down, .start:
-            return 0b1000
+        case .right:
+            return .right
+        case .left:
+            return .left
+        case .up:
+            return .up
+        case .down:
+            return .down
+        case .a:
+            return .a
+        case .b:
+            return .b
+        case .select:
+            return .select
+        case .start:
+            return .start
         }
     }
 }
 
-extension Joypad.SelectedKeys {
-    var selectionMask: Byte {
-        switch self {
-        case .directionKeys:
-            return 0b01_0000
-        case .buttonKeys:
-            return 0b10_0000
-        }
-    }
-
-    var allKeysInSelection: Set<Joypad.Key> {
-        switch self {
-        case .directionKeys:
-            return [.right, .left, .up, .down]
-        case .buttonKeys:
-            return [.a, .b, .select, .start]
+extension Set where Element == Joypad.Key {
+    var registerMask: Joypad.Register {
+        reduce(into: []) { register, key in
+            register.formUnion(key.registerMask)
         }
     }
 }
